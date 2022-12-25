dcf36a50883b5d518ac7e81e841b76cbe4227282
Yasir <yasirse1223@hotmail.com>
2022-12-25 19:23:55 +0300

    junk file deleted and comment added to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-// oluşturduğum .jsx'leri import ediyorum
+/* BU PROJENİN ÇALIŞMASI İÇİN node_modules'leri indirmelisiniz ve ardından src klasöründe ApiKey.jsx dosyası oluşturup kendi openweather apinizi , ApiKey() function'ı
+içerisinde oluşturmalısınız. Daha sonra export etmeniz yeterlidir(Dosya ismi doğru olmalı ve function şeklinde olmalı. Api.jsx ve DefaultCitys klasöründeki dosyalarda
+api key kullandım. İsterseniz dosyaların içerisinde oluşturduğum importları ve apikey constlarını silebilir ve yalnızca url içerisine apinizi ekleyebilirsiniz )*/
 import Api from "./components/Api"
 import Istanbul from "./components/DefaultCitys/Istanbul"
 import Ankara from "./components/DefaultCitys/Ankara"
